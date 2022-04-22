1fd7a1e4d70b37c7ca3b60a4f8be24c3e5121446
Aditya <adityajha2103@gmail.com>
2022-04-22 20:37:28 +0530

    More Improvements and added some functions Added Function to open and delete files and some more improvements

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 import os
 import pytube
 from sys import exit
-# from sys import platform
-# PLATFORM:str = platform.lower()
+from sys import platform
+PLATFORM:str = platform.lower()
 
 class YouTube:
     def __init__(self, defaultSavePath:str = "Downloads", fileExtension:str = "mp4") -> None:
         self.defaultSavePath:str = defaultSavePath
         self.fileExtension:str = fileExtension
-
 
         if(os.path.isdir(self.defaultSavePath) == False):
             os.mkdir(self.defaultSavePath)
@@ -17,7 +16,7 @@ class YouTube:
     # List the videos downloaded in the default directory
     def list(self):
         try:
-            videos = os.listdir("Downloads")
+            videos = os.listdir("./Downloads")
             print("\nVideos in Default Directory are: ")
             for vid in videos:
                 print(vid)
@@ -26,8 +25,25 @@ class YouTube:
         finally:
             print()
 
-    def open(self):
-        pass
+    def open(self, fileName: str):
+        open_with = "xdg-open" if PLATFORM != "windows" else "open"
+        if (fileName[0] == "/" or fileName[0].lower() == "c"):
+            os.system(f"{open_with} {fileName}")
+        else:
+            try:
+                os.system(f"{open_with} Downloads/{fileName}")
+            except FileNotFoundError:
+                print("File Not Found!")
+            except Exception as e:
+                print("Unable to open file")
+                print(e)
+
+    def remove(self, fileName: str):
+        try:
+            os.remove(f"{self.defaultSavePath}/{fileName}")
+            print("File Removed Successfully")
+        except Exception as e:
+            print("Unable to remove filel, can only remove file in default directory")
 
     # Downloads a single file
     def download(self, url:str, fileName:str = None, savePath:str = None) -> None:
@@ -40,8 +56,10 @@ class YouTube:
         try:
             yt = pytube.YouTube(url)
             yt.streams.filter(progressive=True, file_extension=self.fileExtension).order_by('resolution').desc().first().download(savePath, filename=f"{fileName}.{self.fileExtension}")
+            return 1
         except Exception as e:
             print(e)
+            return 0
 
     # Downloads multiple file
     # Need path for the list of file links
@@ -53,31 +71,48 @@ class YouTube:
                 os.mkdir(savePath)
         except Exception as e:
             print(e)
-        with open(filePath, "r") as f:
-            for line in f:
-                try:
-                    fileName = line.split("=")[0]
-                    if(fileName == " " or fileName == "" or fileName == line):
+        
+        try:
+            with open(filePath, "r") as f:
+                for line in f:
+                    try:
+                        fileName = line.split("=")[0]
+                        if(fileName == " " or fileName == "" or fileName == line):
+                            fileName = line.split("/")[-1]
+                    except:
                         fileName = line.split("/")[-1]
-                except:
-                    fileName = line.split("/")[-1]
-                try:
-                    url = line.split("=")[1]
-                except:
-                    url = line
-                print(f"Url: {url} \t FileName: {fileName} \t savePath: {savePath}")
-                self.download(url, fileName, savePath)
+                    try:
+                        url = line.split("=")[1]
+                    except:
+                        url = line
+                    print(f"Downloading: \nUrl: {url} \tFileName: {fileName} \t savePath: {savePath}")
+                    try:
+                        fileName = fileName.strip()
+                        self.download(url, fileName, savePath)
+                    except:
+                        print("Unable to download")
+                        print(f"Url: {url} \tFileName: {fileName} \t savePath: {savePath}")
+                        print("Skipping Download...\n")
+        except FileNotFoundError:
+            print("File Not Found!\n")
     
     def menu(self):
         print("\n0. Exit")
         print("1. List Downloaded Videos in Default Directory")
         print("2. Download Single File")
         print("3. Download Multiple Files")
+        print("4. Open File")
+        print("5. Remove Files in default directory")
         print("99. Show this Menu\n")
 
     def CLI(self):
         self.menu()
-        while((user_input:=int(input(">> ")))):
+        while((user_input:=input(">> "))):
+            try:
+                user_input = int(user_input)
+            except ValueError:
+                print("Invalid Input, input should be an integer from the given list")
+                continue
             if(user_input == 0): exit(0)
             elif(user_input == 1): self.list()
             elif(user_input == 2):
@@ -87,8 +122,8 @@ class YouTube:
                 savePath = input("Enter Save Path for the video(Default=None): ")
                 savePath = None if savePath == "" else savePath
                 print("Downloading...")
-                self.download(url, fileName, savePath)
-                print("Downloaded Successfully...")
+                status = self.download(url, fileName, savePath)
+                print("Downloaded Successfully...") if status else print("Unable to download...")
             elif(user_input == 3):
                 filePath = input("Enter File path for the list: ")
                 if(filePath == ""):
@@ -97,14 +132,15 @@ class YouTube:
                 savePath = input("Enter The Save Path for the file: ")
                 savePath = None if savePath=="" else savePath
                 self.downloadMultiple(filePath, savePath)
+            elif(user_input == 4): 
+                fileName = input("Enter File Name: ")
+                self.open(fileName)
+            elif(user_input == 5):
+                fileName = input("Enter File Name: ")
+                self.remove(fileName)
             elif(user_input == 99): self.menu()
+            else: print("Invalid Input")
 
 if __name__ == '__main__':
     y = YouTube()
     y.CLI()
-    # link = str(input('Enter Youtube Video Link: '))
-    # print("Downloading...")
-    # y.download(link)
-    # y.list()
-    # print("Download Successful !")
-    # # y.downloadMultiple("/tmp/test.txt")
